f8f35aa7ff0bc7a448b8ad8814bafbd950f261e7
diego francisco <diego444x@gmail.com>
2023-08-19 21:52:15 -0300

    inputCard

--- mian.js
+++ mian.js
@@ -1,11 +1,33 @@
 const res = document.getElementById("res")
 const btn = document.getElementById("btn")
 
-function newCard(){
+
+function save(w){
+    var tt  = `txt${w}`
+    var txt = document.getElementById(tt)
+    var text = txt.value
+    console.log(text)
+}
+
+var e = 0
+
+function newCard(i){
+    e++
     res.innerHTML += `<div class="cardCreate">
-                        <h1>Nova lista</h1>
-                        <input type="text" id="txt" placeholder="tarefa...">
-                      </div>`
+        <h1>Nova lista</h1>
+        <input type="text" id="txt${e}" onchange="save(${e})" placeholder="tarefa...">
+        <div style="display: flex; flex-direction: row; margin: 5px;">
+            <input type="checkbox" id="check"><p>lasquei</p> 
+        </div>
+        <div style="display: flex; flex-direction: row; margin: 5px;">
+            <input type="checkbox" id="check"><p>lasquei</p> 
+        </div>
+        <div style="display: flex; flex-direction: row; margin: 5px;">
+            <input type="checkbox" id="check"><p>lasquei</p> 
+        </div>
+      </div>`
+
+      console.log(e)
 }
 
 btn.onclick = newCard
